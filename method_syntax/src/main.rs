3730d8e89e9c4c9c98f02e3be05f9401f03f6c55
postables <postables@rtradetechnologies.com>
2020-03-23 14:22:22 -0700

    more updates

--- method_syntax/src/main.rs
+++ method_syntax/src/main.rs
@@ -6,15 +6,38 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn square(size: i32) -> Rectangle {
+        return Rectangle { width: size, height: size };
+    }
     fn area(&self) -> i32 {
         return self.width * self.height;
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        if self.width > other.width && self.height > other.height {
+            return true;
+        }
+        return false;
+    }
+    fn is_square(&self) -> bool {
+        if self.width == self.height {
+            return true;
+        }
+        return false;
     }
 }
 
 fn main() {
     let rect1 = Rectangle { width: 100, height: 125 };
+    let rect2 = Rectangle { width: 50, height: 50};
+    let rect3 = Rectangle { width: 1000, height: 1000};
+    let rect4 = Rectangle::square(10);
     println!(
         "the area of the rectangle is {}",
         rect1.area()
-    )
+    );
+    println!("rect1 can hold rect2 {}", rect1.can_hold(&rect2));
+    println!("rect1 can hold rect3 {}", rect1.can_hold(&rect3));
+    println!("rect1 can hold rect4 {}", rect1.can_hold(&rect4));
+    println!("rect1 is square {}", rect1.is_square());
+    println!("rect4 is square {}", rect4.is_square());
 }
