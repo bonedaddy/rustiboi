34242136373965da0e7b594300f57672690870a5
postables <postables@rtradetechnologies.com>
2020-03-22 23:41:43 -0700

    udates

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -8,11 +8,13 @@ fn main() {
     let mut node1 = build_node(String::from("hello world"));
     println!("id = {}, active_counts = {}, active = {}", node1.id, node1.active_counts, node1.active);
     println!("{}", node1.id);
+    let s = &node1.id;
     let node2 = Node {
-        id: node1.id,
+        id: s.to_string(),
         active_counts: 100,
         active: false,
-    };
+    };    
+    println!("id = {}, active_counts = {}, active = {}", node1.id, node1.active_counts, node1.active);
     node1.id = String::from("new");
     println!("id = {}, active_counts = {}, active = {}", node2.id, node2.active_counts, node2.active);
     println!("id = {}, active_counts = {}, active = {}", node1.id, node1.active_counts, node1.active);
