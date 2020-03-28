b36f0a8ad98f2955ad3f2e9757e3294e8aac6d7e
postables <postables@rtradetechnologies.com>
2020-03-28 01:04:36 -0700

    updates

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -11,9 +11,25 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        // this takes us to the crate root
+        // to access the serve_order call
+        super::serve_order();
+        // alternatively we can do
+        crate::serve_order();
+    }
+    fn cook_order() {}
+}
+
+
+fn serve_order() {}
+
 pub fn eat_at_restaurant() {
     // absolute path
     crate::front_of_house::hosting::add_to_waitlist();
     // relative
     front_of_house::hosting::add_to_waitlist();
 }
+
